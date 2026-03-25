c723d1f60dc93e966f2f902db46f332f3ae32e87
tfla0804 <tfla0804@gmail.com>
2026-03-25 10:46:25 -0300

    first commit

---
+++ src/views/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer id="footer">
+      <p>© 2026 Thalisson21</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/views/pages/projects/Projects.jsx
@@ -0,0 +1,10 @@
+function Projects() {
+    return (
+        <div>
+            <h1>Meus Projetos</h1>
+            <p>Esta é a página de projetos.</p>
+        </div>
+    );
+}
+
+export default Projects;
